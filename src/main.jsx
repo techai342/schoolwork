99ee3e3b9ccde28478ce3c7fbf124dcaf396d762
techai342 <tiktokusr4400@gmail.com>
2025-11-08 19:18:49 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import CalculatorPage from "./components/CalculatorPage.jsx"; // ✅ NEW PAGE
 import "./index.css";
+
+import { Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/calculator" element={<CalculatorPage />} /> {/* ✅ FULL SCREEN CALCULATOR */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
