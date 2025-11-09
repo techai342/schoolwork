6c6be844683fb8666dc69af25126c92ee99c7a04
techai342 <tiktokusr4400@gmail.com>
2025-11-09 09:13:47 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import ScientificCalculatorPage from "./pages/ScientificCalculatorPage.jsx"; // ✅ Correct path
+import ScientificCalculatorPage from "./pages/ScientificCalculatorPage.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/calculator" element={<ScientificCalculatorPage />} /> {/* ✅ Correct route */}
+        {/* Main App Routes */}
+        <Route path="/*" element={<App />} />
+
+        {/* Calculator Page */}
+        <Route path="/calculator" element={<ScientificCalculatorPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
