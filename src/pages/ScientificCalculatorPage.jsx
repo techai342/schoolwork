75443f6ec00e030910292498441ac419083ce7af
techai342 <tiktokusr4400@gmail.com>
2025-11-09 11:00:01 +0500

    Update ScientificCalculatorPage.jsx

--- src/pages/ScientificCalculatorPage.jsx
+++ src/pages/ScientificCalculatorPage.jsx
@@ -2,14 +2,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ScientificCalculator from "../components/ScientificCalculator";
-import Header from "../components/Header";
 
 export default function ScientificCalculatorPage() {
   return (
     <div className="min-h-screen bg-[var(--ios-bg)] text-gray-900 dark:text-white font-inter transition-all duration-300">
       <div className="container mx-auto max-w-4xl px-5 py-8">
-        <Header />
-        
         {/* Back to Home Link */}
         <div className="mb-6">
           <Link 
